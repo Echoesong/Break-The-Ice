1c788769e19b7a29672e2a6597033fae1c2c80d7
Joseph Felker <jayefelker@gmail.com>
2023-03-28 13:46:02 -0500

    add .create route

--- controllers/icebreakers.js
+++ controllers/icebreakers.js
@@ -2,7 +2,8 @@ const Icebreaker = require('../models/Icebreaker')
 
 module.exports = {
     new: newIcebreaker,
-    index
+    index,
+    create
 
 }
 
@@ -14,4 +15,20 @@ function newIcebreaker(req, res){
 
 function index(req, res) {
     res.render('icebreakers/index', { title: 'All Icebreakers'})
+}
+
+function create(req, res){
+    // What am I doing here?
+    // Incoming request that has a data packet of the data
+    // Take the incoming data, use it to populate the schema that we pull from the Model
+    Icebreaker.create(req.body)
+    .then(function (newIcebreaker){
+        // Eventually, it would be ideal to route to '/icebreakers/${newIcebreaker._id}. For now route back to index
+        console.log(newIcebreaker)
+        res.redirect('/')
+    })
+    .catch(function (err){
+        console.log(err)
+        res.redirect('/')
+    })
 }
